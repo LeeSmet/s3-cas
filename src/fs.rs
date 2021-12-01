5b35f3a0ee690fb1339a5c1bc60d2de847596257
Lee Smet <lee.smet@hotmail.com>
2021-12-01 16:59:49 +0100

    Start refcount handling
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/fs.rs
+++ src/fs.rs
@@ -381,16 +381,24 @@ impl TryFrom<&[u8]> for Object {
 struct Block {
     size: usize,
     path: Vec<u8>,
+    #[cfg(refcount)]
+    rc: usize,
 }
 
 impl From<&Block> for Vec<u8> {
     fn from(b: &Block) -> Self {
         // NOTE: we encode the lenght of the vector as a single byte, since it can only be 16 bytes
         // long.
+        #[cfg(not(refcount))]
         let mut out = Vec::with_capacity(PTR_SIZE + b.path.len() + 1);
+        #[cfg(refcount)]
+        let mut out = Vec::with_capacity(2 * PTR_SIZE + b.path.len() + 1);
+
         out.extend_from_slice(&b.size.to_le_bytes());
         out.extend_from_slice(&(b.path.len() as u8).to_le_bytes());
         out.extend_from_slice(&b.path);
+        #[cfg(refcount)]
+        out.extend_from_slice(&b.rc.to_le_bytes());
         out
     }
 }
@@ -403,15 +411,29 @@ impl TryFrom<&[u8]> for Block {
             return Err(FsError::MalformedObject);
         }
         let size = usize::from_le_bytes(value[..PTR_SIZE].try_into().unwrap());
+
         let vec_size =
             u8::from_le_bytes(value[PTR_SIZE..PTR_SIZE + 1].try_into().unwrap()) as usize;
+        if value.len() < PTR_SIZE + 1 + vec_size {
+            return Err(FsError::MalformedObject);
+        }
+        let path = value[PTR_SIZE + 1..PTR_SIZE + 1 + vec_size].to_vec();
+
+        #[cfg(not(refcount))]
         if value.len() != PTR_SIZE + 1 + vec_size {
+            return Err(FsError::MalformedObject);
+        }
+
+        #[cfg(refcount)]
+        if value.len() != PTR_SIZE * 2 + 1 + vec_size {
             return Err(FsError::MalformedObject);
         }
 
         Ok(Block {
             size,
-            path: value[PTR_SIZE + 1..].to_vec(),
+            path,
+            #[cfg(refcount)]
+            rc: usize::from_le_bytes(value[PTR_SIZE + 1 + vec_size..]),
         })
     }
 }
